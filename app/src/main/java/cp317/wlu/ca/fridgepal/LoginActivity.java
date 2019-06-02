0be067942f9b343796b4348c20fdb0e03dd70e5b
Ama Liyanage <liyaniama@gmail.com>
2019-06-02 10:40:17 -0400

    Create a basic application with the first screens.
    Added:
    - SplashActivity
    - LoginActivity
    - MainActivity

---
+++ app/src/main/java/cp317/wlu/ca/fridgepal/LoginActivity.java
@@ -0,0 +1,20 @@
+package cp317.wlu.ca.fridgepal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/cp317/wlu/ca/fridgepal/SplashActivity.java
@@ -0,0 +1,21 @@
+package cp317.wlu.ca.fridgepal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
