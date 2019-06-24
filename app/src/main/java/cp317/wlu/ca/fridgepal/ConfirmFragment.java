bdc003abd2a2bde570d4dfd6b63cd4db662b2528
Ama Liyanage <liyaniama@gmail.com>
2019-06-24 19:16:33 -0400

    create ConfirmFragment
    When confirm button is pressed go to main activity.

--- app/src/main/java/cp317/wlu/ca/fridgepal/ConfirmFragment.java
+++ app/src/main/java/cp317/wlu/ca/fridgepal/ConfirmFragment.java
@@ -1,11 +1,15 @@
 package cp317.wlu.ca.fridgepal;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
 
 
 /**
@@ -18,12 +22,26 @@ public class ConfirmFragment extends Fragment {
         // Required empty public constructor
     }
 
+    interface onNextPressedListener {
+        void onNextPressed(View view);
+    }
+
+    private onNextPressedListener onNextPressedListener;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirm, container, false);
+        View view = inflater.inflate(R.layout.fragment_confirm, container, false);
+
+
+        Button nextButton = view.findViewById(R.id.button4);
+        nextButton.setOnClickListener(onNextPressedListener::onNextPressed);
+        return view;
     }
 
+    public void setOnNextPressedListener(onNextPressedListener onNextPressedListener) {
+        this.onNextPressedListener = onNextPressedListener;
+    }
 }
